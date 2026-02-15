c470413f8f3d3f530823b5585a9e9baadc806aba
Mai Khanh <mktran28@colby.edu>
2026-02-15 13:30:34 -0500

    Experiment with floating point representation

---
+++ extension2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *args[]) {
+    float x = 99999999999999998.0;
+    float y = 99999999999999999.0;
+
+    unsigned char *xptr = (unsigned char *)&x;
+    unsigned char *yptr = (unsigned char *)&y;
+
+    for (int i = 0; i < sizeof(float); i++) {
+        printf("%d: %02X, %02X\n", i, xptr[i], yptr[i]);
+    }
+
+    return 0;
+}
