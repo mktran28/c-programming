58f447abe72cbf22eb81586bb90e1bd2a8daa60e
Mai Khanh <mktran28@colby.edu>
2026-02-17 21:23:28 -0500

    Add the mathematical basis of floating point representation of very large numbers in extension2.x

--- extension2.c
+++ extension2.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 int main (int argc, char *args[]) {
+    // Any numbers larger than 2^24 (so being multiplied by 2^-23 won't make a different)
     float x = 99999999999999998.0;
     float y = 99999999999999999.0;
 
